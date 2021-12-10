b70488aefa866ad6ea50be9c012ed0368b195d8d
Ankur Dhuriya <mail.ankurdhuriya@gmail.com>
2021-12-10 12:53:59 +0530

    Update infer.sh

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -1,6 +1,7 @@
 glowdir=''
 hifidir=''
 device=''
+text=''
 wav=''
 
-python ../src/glow_tts/texttospeech.py -m $glowdir -g $hifidir -d $device -w $wav
+python ../src/glow_tts/texttospeech.py -m $glowdir -g $hifidir -d $device -t $text -w $wav
