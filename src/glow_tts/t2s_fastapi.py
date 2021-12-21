2c9fda161a17c06664592952adb8c54333575dec
Ankur Dhuriya <mail.ankurdhuriya@gmail.com>
2021-12-21 11:39:18 +0530

    Update t2s_fastapi.py

--- src/glow_tts/t2s_fastapi.py
+++ src/glow_tts/t2s_fastapi.py
@@ -1,5 +1,5 @@
 from starlette.responses import StreamingResponse
-from texttospeech import MelToWav, TextToMel, TextToSpeech
+from texttospeech import MelToWav, TextToMel
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
