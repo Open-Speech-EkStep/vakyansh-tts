80e2ecdfcc1b72cc3fec59c89b9d1feaa59f30be
harveen <harveensinghchadha@gmail.com>
2022-02-22 14:23:55 +0530

    |Harveen| Fixes for hifi

--- scripts/hifi/train_hifi.sh
+++ scripts/hifi/train_hifi.sh
@@ -1,10 +1,21 @@
+#!/bin/bash
+
+gender='male'
+
+config='../../config/hifi/config_v1.json'
+modeldir='../../checkpoints/hifi/'$gender
+logdir='../../logs/hifi/'$gender
+
+
+####################################################
+
+
+
 python ../src/hifi_gan/train.py \
-    --config '' \
-    --input_wavs_dir '' \
-    --input_mels_dir '' \
-    --input_training_file '' \
-    --input_validation_file '' \
-    --checkpoint_path '' \
-    --logs_path '' \
+    --config $config \
+    --input_training_file '../../data/hifi/'$gender'/train.txt' \
+    --input_validation_file '../../data/hifi/'$gender'/valid.txt' \
+    --checkpoint_path $modeldir \
+    --logs_path $logdir \
     --checkpoint_interval 10000 \
     --stdout_interval 50
