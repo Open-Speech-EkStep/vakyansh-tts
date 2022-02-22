6e549ca715cc14cbe100d69aaf44a8c4b05e290b
harveen <harveensinghchadha@gmail.com>
2022-02-22 14:29:06 +0530

    |Harveen| inference script

--- scripts/inference/infer.sh
+++ scripts/inference/infer.sh
@@ -1,7 +1,11 @@
-glowdir=''
-hifidir=''
-device=''
-text=''
-wav=''
 
+gender='male'
+glowdir='../../checkpoints/glow/'$gender'/'
+hifidir='../../checkpoints/hifi/'$gender'/'
+device='cpu'
+text='testing this one'
+wav='../../results/'$gender
+
+
+mkdir -p $wav
 python ../src/glow_tts/texttospeech.py -m $glowdir -g $hifidir -d $device -t $text -w $wav
