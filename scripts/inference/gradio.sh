67ed8ee381b825f38dc4363b1e9788b492f0ebb3
harveen <harveensinghchadha@gmail.com>
2022-02-24 16:59:09 +0530

    |Harveen| Fixing Gradio inference

--- scripts/inference/gradio.sh
+++ scripts/inference/gradio.sh
@@ -2,7 +2,7 @@ gender='male'
 glowdir='../../checkpoints/glow/'$gender'/'
 hifidir='../../checkpoints/hifi/'$gender'/'
 device='cpu'
-lang='hi'
+lang='en'
 
 
 python ../../utils/inference/gradio.py -a $glowdir -v $hifidir -d $device -L $lang
